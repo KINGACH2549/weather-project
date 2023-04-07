a57ef8595aac9a20434f16d8a5a7ac12a34ef73b
Achintya Mishra <achintyammishra2000@gmail.com>
2023-04-07 16:35:29 +0530

    changed request policy

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.set("views", viewPath);
 
 app.use(express.static(publicDirectory)); //Serving static contents
 app.use(function (req, res, next) {
-  req.setHeader("Referrer-Policy", "unsafe-url");
+  req.header("Referrer-Policy", "unsafe-url");
   res.setHeader("Referrer-Policy", "unsafe-url");
   next();
 });
